f68e3c39cc3a68c37fe1e5deef6da9bd7d1d2ad0
Luma <rdslum@gmail.com>
2024-12-08 15:02:01 -0300

    Adding new environment variable

--- zapsign/wsgi.py
+++ zapsign/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zapsign.settings')
 
 application = get_wsgi_application()
+
+
+if os.getenv("RUN_MIGRATIONS", "false").lower() == "true":
+    try:
+        call_command("migrate")
+    except Exception as e:
+        print(f"Erro ao rodar migrações: {e}")
